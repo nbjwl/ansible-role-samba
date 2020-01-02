30feeb4e3a0c99cee8e6084fc322ad3ff70f820d
nbjwl <nbjwl@live.com>
2020-01-02 22:33:06 +0800

    ansible role samba
    support debian 9 and 10

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+@pytest.mark.parametrize("command", [
+    "systemctl status smb"
+])
+def test_nginx_config(host, command):
+    assert host.run(command).rc == 0
